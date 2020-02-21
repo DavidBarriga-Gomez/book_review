f080da4d5c7da82bcf85039c6e11ceebbe828c9d
David Gomez <davdg25@gmail.com>
2020-02-21 10:39:44 -0700

    display author title and genre

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,12 @@ class SearchController < ApplicationController
   def index
     connection = Faraday.new(url: 'http://openlibrary.org') do |faraday|
       faraday.headers
-      binding.pry
     end
 
     response = connection.get('/search.json?title=the+man+who+saw+everything')
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    
+    @book = json[:docs].first
   end
 end
